b02c4070a220fb36bbaad3175c791362b35f7ec2
HassanMekhkamer <hassan.mekhamer.97@gmail.com>
2024-12-27 18:52:35 +0200

    working tic-tac-toe with a basic design (no draw)

--- script.js
+++ script.js
@@ -2,34 +2,34 @@
 const main = document.querySelector(".main");
 const score = document.querySelector(".score");
 const board = document.querySelector(".board");
+const namesBtn = document.querySelector(".namesBtn");
+const xInput = document.querySelector("#xName");
+const oInput = document.querySelector("#oName");
+const resetBtn = document.querySelector(".resetBtn");
+const pXscore = document.querySelector(".player1");
+const pOscore = document.querySelector(".player2");
+//Text shown when a player wins
+const winText = document.createElement("p");
 
-//players' scores variables
-let pXScore = document.querySelector(".player1");
-let pOscore = document.querySelector(".player2");
-
-// //player names from inputs
-// let xName = document.querySelector("#xName").value;
-// let oName = document.querySelector("#oName").value;
-
-
-
-//Player creation with name and score = 0
-function createPlayer(name) {
-    return { name, score: 0 }
-}
-let playerX = createPlayer("Hassan");
-let playerO = createPlayer("Jimmy");
-
-pXScore.textContent = `${playerX.name}: ${playerX.score}`
-pOscore.textContent = `${playerO.name}: ${playerO.score}`
-
-let gameOver = false;
-
-
-
+//Main game object
 let gameObj = {
     gameBoard: ["", "", "", "", "", "", "", "", "",]
 }
+
+//Button to submit names entered
+namesBtn.addEventListener("click", () => {
+    if (xInput.value !== "") {
+        playerX.name = xInput.value;
+    }
+    if (oInput.value !== "") {
+        playerO.name = oInput.value;
+    }
+    pXscore.textContent = `${playerX.name}: ${playerX.score}`
+    pOscore.textContent = `${playerO.name}: ${playerO.score}`
+
+    xInput.value = "";
+    oInput.value = "";
+})
 
 //adding 9 cells to the board
 for (let i = 0; i < gameObj.gameBoard.length; i++) {
@@ -40,116 +40,86 @@ for (let i = 0; i < gameObj.gameBoard.length; i++) {
     board.appendChild(cell);
 }
 
+//Player creation objects (name, score = 0)
+function createPlayer(name) {
+    return { name, score: 0 }
+}
+let playerX = createPlayer("Player X");
+let playerO = createPlayer("Player O");
+
+////Game over or not
+let gameOver = false;
+
 //creating an array of all 9 cells
 let cells = document.querySelectorAll(".cell");
 
 //either X or O switches each click
 let currentPlayer = "X";
 
-//function to add X or O when clicking a cell
+const winningCombos = [
+    [0, 1, 2],
+    [0, 3, 6],
+    [0, 4, 8],
+    [1, 3, 7],
+    [2, 5, 8],
+    [3, 4, 5],
+    [6, 7, 8],
+    [2, 4, 6]
+];
+
+//Function to run the game
 function marker(cell) {
     if (cell.textContent === "" && !gameOver) {
         cell.textContent = currentPlayer;
         currentPlayer = currentPlayer === "X" ? "O" : "X";
-        console.log("this works  at cell " + cell.dataset.index)
         gameObj.gameBoard[cell.dataset.index] = cell.textContent;
-    } else {
-        console.log("Cell already taken!")
-
     }
-    if ((cell.textContent !== "") && (
-        //top row
-        gameObj.gameBoard[0] === gameObj.gameBoard[1] &&
-        gameObj.gameBoard[1] === gameObj.gameBoard[2] &&
-        (gameObj.gameBoard[0] === "X" || gameObj.gameBoard[0] === "O")
-        ||
-        //left column
-        gameObj.gameBoard[0] === gameObj.gameBoard[3] &&
-        gameObj.gameBoard[3] === gameObj.gameBoard[6] &&
-        (gameObj.gameBoard[0] === "X" || gameObj.gameBoard[0] === "O")
-        ||
-        //first diagonal
-        gameObj.gameBoard[0] === gameObj.gameBoard[4] &&
-        gameObj.gameBoard[4] === gameObj.gameBoard[8] &&
-        (gameObj.gameBoard[0] === "X" || gameObj.gameBoard[0] === "O")
+    if (cell.textContent !== "") {
+        console.log("cell already taken!")
+    }
 
-        ||
-        //mid column
-        gameObj.gameBoard[1] === gameObj.gameBoard[4] &&
-        gameObj.gameBoard[4] === gameObj.gameBoard[7] &&
-        (gameObj.gameBoard[1] === "X" || gameObj.gameBoard[1] === "O")
-
-        ||
-        //right column
-        gameObj.gameBoard[2] === gameObj.gameBoard[5] &&
-        gameObj.gameBoard[5] === gameObj.gameBoard[8] &&
-        (gameObj.gameBoard[2] === "X" || gameObj.gameBoard[2] === "O")
-
-        ||
-        //mid row
-        gameObj.gameBoard[3] === gameObj.gameBoard[4] &&
-        gameObj.gameBoard[4] === gameObj.gameBoard[5] &&
-        (gameObj.gameBoard[3] === "X" || gameObj.gameBoard[3] === "O")
-        ||
-        //bottom row
-        gameObj.gameBoard[6] === gameObj.gameBoard[7] &&
-        gameObj.gameBoard[7] === gameObj.gameBoard[8] &&
-        (gameObj.gameBoard[6] === "X" || gameObj.gameBoard[6] === "O")
-        ||
-        //other diagonal
-        gameObj.gameBoard[2] === gameObj.gameBoard[4] &&
-        gameObj.gameBoard[4] === gameObj.gameBoard[6] &&
-        (gameObj.gameBoard[2] === "X" || gameObj.gameBoard[2] === "O")
-    )
-    ) {
+    function checkWinner() {
         gameOver = true;
 
         if (gameObj.gameBoard[cell.dataset.index] === "X") {
             playerX.score++;
-            pXScore.textContent = `${playerX.name}: ${playerX.score}`
-            console.log(`Game over ${playerX.name} wins`)
-
-        } else if (gameObj.gameBoard[cell.dataset.index] === "O") {
+            pXscore.textContent = `${playerX.name}: ${playerX.score}`
+            console.log(`${playerX.name} wins`)
+            gameObj.gameBoard = ["", "", "", "", "", "", "", "", "",];
+            winText.textContent = `${playerX.name} wins! reset board to play again`
+            main.appendChild(winText)
+        }
+        if (gameObj.gameBoard[cell.dataset.index] === "O") {
             playerO.score++;
             pOscore.textContent = `${playerO.name}: ${playerO.score}`
-            console.log(`Game over ${playerO.name} wins`)
-
+            console.log(`${playerO.name} wins`)
+            gameObj.gameBoard = ["", "", "", "", "", "", "", "", "",];
+            winText.textContent = `${playerO.name} wins! reset board to play again`
+            main.appendChild(winText)
         }
     }
 
+    for (combo of winningCombos) {
+        let [a, b, c] = combo
 
-    if (gameOver) {
-        gameObj.gameBoard = ["", "", "", "", "", "", "", "", "",];
-        cells.forEach((cell) => { cell.textContent = "" })
-        gameOver = false;
+        if (
+            gameObj.gameBoard[a] &&
+            gameObj.gameBoard[a] == gameObj.gameBoard[b] &&
+            gameObj.gameBoard[b] == gameObj.gameBoard[c]
+        ) {
+            checkWinner()
+        }
     }
 
 }
-
 
 cells.forEach((cell) => {
     cell.addEventListener("click", () => { marker(cell) })
 })
 
-if (gameOver) {
-    cells.forEach((cell) => {
-        cell.removeEventListener("click", () => { marker(cell) })
-    })
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+resetBtn.addEventListener("click", (target) => {
+    gameObj.gameBoard = ["", "", "", "", "", "", "", "", "",];
+    cells.forEach((cell) => { cell.textContent = "" })
+    gameOver = false;
+    winText.textContent = "";
+})
